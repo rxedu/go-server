744973e1237ab0f4dcb17d1934bc70ef3c5de7f9
Evan Sosenko <razorx@evansosenko.com>
2022-09-01 22:49:57 -0500

    Add server

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"github.com/rxedu/go-server/v1/pkg"
+	"fmt"
+	"log"
+	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "404", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+	}
+	fmt.Fprintf(w, "POST ok")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
 func main() {
-	pkg.PrintMessage()
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Printf("Go! http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
